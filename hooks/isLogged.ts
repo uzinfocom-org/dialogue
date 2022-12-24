006ea935e6a249aa19b70de317bd1296d3a56d18
UwUssimo Robinson <uwussimo@icloud.com>
2022-12-24 16:45:15 +0500

    [RM] removing unused modules

--- hooks/isLogged.ts
+++
@@ -1,8 +0,0 @@
-import env from "../utils/config.ts";
-import { Context, NextFunction } from "../deps.ts";
-
-export default async (ctx: Context, next: NextFunction) => {
-  await ctx.forwardMessage(env["FORWARD"]);
-
-  await next();
-};
